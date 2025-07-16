a6bb1d606edfb000f639536fb63a430c9a39bc5b
VadnagariShubham <shubhvadnagari@gmail.com>
2025-07-16 21:23:49 +0530

    Add test for adding sweet

--- src/sweetshop.js
+++ src/sweetshop.js
@@ -9,18 +9,20 @@ class sweetshop {
     if (this.sweets[id]) {
       throw new Error("Sweet already exists");
     }
-    // Validate price is numeric
+   
     if (typeof price !== 'number' || isNaN(price)) {
       throw new Error('Price must be a valid number');
     }
 
-    // Validate quantity is not negative
+   
     if (quantity < 0) {
       throw new Error('Quantity cannot be negative');
     }
 
     this.sweets[id] = new sweet(id,name,category,price,quantity);
   }
+
+  
 
 
 
--- test/sweetShop.test.js
+++ test/sweetShop.test.js
@@ -6,10 +6,14 @@ describe('SweetShop', () => {
   beforeEach(() => {
     shop = new SweetShop();
   });
-
+   //this test cases are for addSweets
    test('throws error on duplicate sweet ID', () => {
     shop.addSweet('1001', 'Kaju Katli', 'Nut-Based', 50, 20);
     expect(() => shop.addSweet('1001', 'gajar halva', 'Vegetable-Based', 30, 15)).toThrow();
+  });
+   test('adds a sweet', () => {
+    shop.addSweet('1001', 'Kaju Katli', 'Nut-Based', 50, 20);
+    expect(shop.sweets['1001'].name).toBe('Kaju Katli');
   });
 
 
