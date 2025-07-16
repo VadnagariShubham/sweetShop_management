e8d9abb37a460a19e9b9dd8ed4bb8e730cd197d0
VadnagariShubham <shubhvadnagari@gmail.com>
2025-07-16 21:17:21 +0530

    Add test for adding sweets

--- src/sweetshop.js
+++ src/sweetshop.js
@@ -1,13 +1,28 @@
-const sweet = require('../data/sweet');
+const sweet = require('../src/sweet');
 
 class sweetshop {
   constructor() {
     this.sweets = {}
   }
-addSweet(id, name, category, price, quantity) {
-    const newSweet = new sweet(id, name, category, price, quantity);
-    this.sweets[id] = newSweet;
+  addSweet(id, name, category, price, quantity) {
+
+    if (this.sweets[id]) {
+      throw new Error("Sweet already exists");
+    }
+    // Validate price is numeric
+    if (typeof price !== 'number' || isNaN(price)) {
+      throw new Error('Price must be a valid number');
+    }
+
+    // Validate quantity is not negative
+    if (quantity < 0) {
+      throw new Error('Quantity cannot be negative');
+    }
+
+    this.sweets[id] = new sweet(id,name,category,price,quantity);
   }
+
+
 
 }
 
--- test/sweetShop.test.js
+++ test/sweetShop.test.js
@@ -1,10 +1,15 @@
-const SweetShop = require('../controller/sweetshop');
+const SweetShop = require('../src/sweetshop');
 
 describe('SweetShop', () => {
   let shop;
 
   beforeEach(() => {
     shop = new SweetShop();
+  });
+
+   test('throws error on duplicate sweet ID', () => {
+    shop.addSweet('1001', 'Kaju Katli', 'Nut-Based', 50, 20);
+    expect(() => shop.addSweet('1001', 'gajar halva', 'Vegetable-Based', 30, 15)).toThrow();
   });
 
 
